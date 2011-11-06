f37e4ddf05e1d990de855424683c5b757dbf4e5f
DJ Chung <djchung3@gmail.com>
2011-11-05 23:27:15 -0700

    local settings

--- settings.py
+++ settings.py
@@ -1,24 +1,10 @@
 # Django settings for djblog project.
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
-
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
 )
 
 MANAGERS = ADMINS
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': './djblog.sqlite',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -141,3 +127,9 @@ LOGGING = {
         },
     }
 }
+
+
+try:
+	from local_settings import *
+except ImportError:
+	pass
